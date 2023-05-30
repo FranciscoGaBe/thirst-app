3831031d572d6e425dcb26f6d93d0dd35ff8edd0
FranciscoGarrido <francisco.garrido.bear@gmail.com>
2023-05-30 23:37:16 +0200

    API: add getById to drink repository

--- api/src/domain/drink.repository.ts
+++ api/src/domain/drink.repository.ts
@@ -2,5 +2,6 @@ import { Drink } from "./drink.entity"
 
 export type DrinkReposity = {
   getAll: () => Drink[],
+  getById: (id: Drink['id']) => Drink | null,
   update: (id: Drink['id'], drink: Partial<Drink>) => Drink,
 }
