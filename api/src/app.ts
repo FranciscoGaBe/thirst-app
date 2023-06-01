ec57446bc00f690ad00972c997cc32f961b9573b
FranciscoGarrido <francisco.garrido.bear@gmail.com>
2023-06-02 01:04:14 +0200

    API: add static storage folder with images

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,12 +2,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import path from 'node:path'
 
 import { loadControllers } from './infrastructure/controllers/loadControllers'
 
 const app = express()
 const port = process.env.PORT ?? 8000
 
+app.use(express.static(path.join(__dirname, '..', 'storage')))
 app.use(bodyParser.json())
 app.use(cors())
 app.set('port', port)
