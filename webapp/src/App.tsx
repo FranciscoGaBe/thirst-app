1b2394ee0a4d34d54e14a63f7227cdb8676ae3c4
FranciscoGarrido <francisco.garrido.bear@gmail.com>
2023-06-01 21:39:07 +0200

    WebApp: remove unnecesary files

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,16 +1,6 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { Header } from "./ui/Header";
-import { Home } from "./ui/Home";
-import styles from './App.module.css';
-
 function App() {
     return (
-        <Router>
-            <div className={styles.App}>
-                <Header />
-                <Home />
-            </div>
-        </Router>
+        <div>App</div>
     )
 }
 
