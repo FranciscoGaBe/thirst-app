ffaeea46b6ff6152a188c69438044d578126d0eb
FranciscoGarrido <francisco.garrido.bear@gmail.com>
2023-06-01 23:04:56 +0200

    WebApp: update eslint rules

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -16,5 +16,14 @@ module.exports = {
     'react'
   ],
   rules: {
+    'max-len': ['warn', 80],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-closing-tag-location': 'warn',
+    'react/jsx-closing-bracket-location': 'warn',
+    'react/jsx-curly-spacing': ['warn', { when: 'always', children: true }],
+    'react/jsx-curly-newline': 'warn',
+    'react/jsx-indent': ['warn', 2],
+    'react/jsx-space-before-closing': 'warn',
+    'react/jsx-wrap-multilines': 'error'
   }
 }
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const test = ''
 ReactDOM.render(
   <React.StrictMode>
+    { test }
     <App />
   </React.StrictMode>,
   document.getElementById('root')
