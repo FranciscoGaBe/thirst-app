ac9a3ae2fc49c88ad786b19dfb968206fcb0c680
FranciscoGarrido <francisco.garrido.bear@gmail.com>
2023-06-05 00:47:04 +0200

    WebApp: migrate index to use createRoot

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { StoreProvider } from './services/store'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root') as HTMLElement)
+root.render(
   <React.StrictMode>
     <StoreProvider>
       <App />
     </StoreProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
