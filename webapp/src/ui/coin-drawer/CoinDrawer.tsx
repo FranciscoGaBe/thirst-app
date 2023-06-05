099f5276fabb3c2f37cf2f3f0fff5c5221daf863
FranciscoGarrido <francisco.garrido.bear@gmail.com>
2023-06-06 00:14:16 +0200

    WebApp: add styles to CoinDrawer

--- webapp/src/ui/coin-drawer/CoinDrawer.tsx
+++ webapp/src/ui/coin-drawer/CoinDrawer.tsx
@@ -1,4 +1,5 @@
 import { useIncreaseMoney } from '../../application/increaseMoney'
+import styles from './CoinDrawer.module.css'
 
 const increaseButtons: number[] = [
   0.5,
@@ -10,18 +11,21 @@ export const CoinDrawer = (): JSX.Element => {
   const { increase } = useIncreaseMoney()
 
   return (
-    <div role="presentation">
-      {
-        increaseButtons.map(increaseAmount => (
-          <button
-              key={ increaseAmount }
-              aria-label={ `Add ${increaseAmount}` }
-              onClick={ () => { increase(increaseAmount) } }
-          >
-            { increaseAmount }
-          </button>
-        ))
-      }
+    <div role="presentation" className={ styles['coin-drawer'] }>
+      <div className={ styles.slot }>&nbsp;</div>
+      <div className={ styles.coins }>
+        {
+          increaseButtons.map(increaseAmount => (
+            <button
+                key={ increaseAmount }
+                aria-label={ `Add ${increaseAmount}` }
+                onClick={ () => { increase(increaseAmount) } }
+            >
+              { increaseAmount }
+            </button>
+          ))
+        }
+      </div>
     </div>
   )
 }
