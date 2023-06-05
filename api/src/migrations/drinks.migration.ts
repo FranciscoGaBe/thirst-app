66cc07f08ee539a230fac8e85f314bb7491840aa
FranciscoGarrido <francisco.garrido.bear@gmail.com>
2023-06-06 00:34:54 +0200

    Api: update drinks migration

--- api/src/migrations/drinks.migration.ts
+++ api/src/migrations/drinks.migration.ts
@@ -41,6 +41,69 @@ const drinksMigration: Migration<DrinkRecord> = ({ createDatabase, addRecord }) 
     image: 'orange-juice.png'
   })
 
+  addRecord(database, {
+    id: 4,
+    name: 'Energy Drink',
+    price: 5,
+    cost: 3,
+    quantity: 1,
+    image: 'energy-drink.png'
+  })
+
+  addRecord(database, {
+    id: 5,
+    name: 'Green Tea',
+    price: 1,
+    cost: 0.4,
+    quantity: 10,
+    image: 'green-tea.png'
+  })
+
+  addRecord(database, {
+    id: 6,
+    name: 'Milk',
+    price: 1,
+    cost: 0.5,
+    quantity: 5,
+    image: 'milk.png'
+  })
+
+  addRecord(database, {
+    id: 7,
+    name: 'Sparkling Water',
+    price: 2,
+    cost: 0.8,
+    quantity: 5,
+    image: 'sparkling-water.png'
+  })
+
+  addRecord(database, {
+    id: 8,
+    name: 'Cocktail',
+    price: 5,
+    cost: 2,
+    quantity: 5,
+    image: 'cocktail.png'
+  })
+
+  addRecord(database, {
+    id: 9,
+    name: 'Lemonade',
+    price: 1,
+    cost: 0.1,
+    quantity: 20,
+    image: 'lemonade.png'
+  })
+
+  addRecord(database, {
+    id: 10,
+    name: 'Chocolate milk',
+    price: 2,
+    cost: 1.4,
+    quantity: 10,
+    image: 'chocolate-milk.png'
+  })
+
   return true
 }
 
